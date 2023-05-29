370f416484173c40dab4a265d485c741da068f65
Karamat Ullah <mail.karamatullah@gmail.com>
2023-05-30 02:36:41 +0500

    basic files and code clean up

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Hunting Coder</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <nav className={styles.mainNav}>
+        <ul>
+          <Link href='/'><li>Home</li></Link>
+          <Link href='/about'><li>About</li></Link>
+          <Link href='/blog'><li>Blog</li></Link>
+          <Link href='/contact'><li>Contact</li></Link>
+        </ul>
+      </nav>
 
       <main>
         <h1 className={styles.title}>
@@ -18,82 +27,36 @@ export default function Home() {
           A Blog for Hunting Coders written by Hunting Coder.
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="blogs">
+          <h2>Popular Blogs</h2>
+          <div className="blogItem">
+            <h3>How to learn js in 2023.</h3>
+            <p>Javascripts is the front end framework that allows us to write client side logic into our websites.</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn js in 2023.</h3>
+            <p>Javascripts is the front end framework that allows us to write client side logic into our websites.</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn js in 2023.</h3>
+            <p>Javascripts is the front end framework that allows us to write client side logic into our websites.</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn js in 2023.</h3>
+            <p>Javascripts is the front end framework that allows us to write client side logic into our websites.</p>
+          </div>
         </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+      </main>
 
       <style jsx>{`
         main {
-          padding: 5rem 0;
+          padding: 2rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
 
