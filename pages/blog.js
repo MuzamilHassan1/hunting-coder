ea82a652c331bcb3ed944feb9e32206c9435a0af
Muzamil Hassan <muzamilhassan1234@gmail.com>
2023-05-31 21:47:26 +0500

    add blog and blogpost page

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
 
 const Blog = () => {
   return (
-    <div>Blog Page</div>
+    <div className={styles.container}>
+      <div className="blogs">
+        <div className="blogItem">
+          <Link className={styles.blogLink} href='/blogpost/learn-js-in-2023'><h3>How to learn js in 2023.</h3></Link>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,.....</p>
+        </div>
+        <div className="blogItem">
+          <Link className={styles.blogLink} href='/blogpost/learn-js-in-2023'><h3>How to learn js in 2023.</h3></Link>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,.....</p>
+        </div>
+        <div className="blogItem">
+          <Link className={styles.blogLink} href='/blogpost/learn-js-in-2023'><h3>How to learn js in 2023.</h3></Link>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,.....</p>
+        </div>
+        <div className="blogItem">
+          <Link className={styles.blogLink} href='/blogpost/learn-js-in-2023'><h3>How to learn js in 2023.</h3></Link>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,.....</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from '../../styles/BlogPost.module.css'
 
 const slug = () => {
   const router = useRouter();
   const { query: { slug } } = router;
 
   return (
-    <div>{slug}</div>
+    <div className={styles.container}>
+      <h2>Page Title {slug}</h2>
+      <hr />
+      <p className='content'>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+       in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+       sunt in culpa qui officia deserunt mollit anim id est laborum."
+      </p>
+    </div>
   )
 }
 
